614063a597f44f37379575bda1e99488def46f91
Dea Nursyakinah <deanursyakinah@gmail.com>
2025-05-12 22:40:21 +0700

    commit12Mei2

--- model/publisher.py
+++ model/publisher.py
@@ -1,19 +1,19 @@
 from model.Account import Account
 
 class Publisher(Account):
-    def __init__(self, name='', password='', id=0):
-        super().__init__(name, password, id, role=Account.Role.PUBLISHER)
+    def __init__(self, name='', email = '',password='', id=0):
+        super().__init__(name, email, password, id, role=Account.Role.PUBLISHER)
         self.published_games = []
         self.published_dlcs = []
 
     def get_published_games(self):
         return self.published_games
 
-    def set_published_games(self, games):
-        self.published_games = games
+    def set_published_games(self, game):
+        self.published_games = game
 
     def get_published_dlcs(self):
         return self.published_dlcs
 
-    def set_published_dlcs(self, dlcs):
-        self.published_dlcs = dlcs
+    def set_published_dlcs(self, dlc):
+        self.published_dlcs = dlc
